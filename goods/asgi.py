4a8e827a05d058a0043b63cb7d575820c16f354e
Alexander Skopintsev <milleniumfalcon_@mail.ru>
2020-07-13 22:55:44 +0300

    Refactoring: - removed additional app folder "goods" - package "avidjango" renamed to "goods"

--- goods/asgi.py
+++ goods/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for avidjango project.
+ASGI config for goods project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avidjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goods.settings')
 
 application = get_asgi_application()
